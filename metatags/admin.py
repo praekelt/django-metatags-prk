d8f1099f501468e3829f0c103eb96a65f222b9ce
Johan Beyers <johan@juizi.com>
2017-02-20 14:57:33 +0200

    Exclude urlmetatags from inlines

--- metatags/admin.py
+++ metatags/admin.py
@@ -15,13 +15,13 @@ admin.site.register(URLMetatag, admin.ModelAdmin)
 
 # Hook the metatag inlines into the admin for selected models
 included_models = []
-excluded_models = []
+excluded_models = ["metatags.urlmetatag"]
 if hasattr(settings, "METATAGS"):
     if "inline_models" in settings.METATAGS:
         included_models = settings.METATAGS["inline_models"]
 
     if "exclude_inline_models" in settings.METATAGS:
-        excluded_models = settings.METATAGS["exclude_inline_models"]
+        excluded_models = settings.METATAGS["exclude_inline_models"] + excluded_models
 
 for model, admin_model in admin.site._registry.items():
     model_type = "%s.%s" % (model._meta.app_label, model._meta.model_name)
